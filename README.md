a3b18b132fe07db01bf17c71955a91920d176c5d
BrianSuhit <braisuhit@gmail.com>
2025-08-19 18:32:16 -0300

    A diorama was added to the project for testing

--- README.md
+++ README.md
@@ -1,42 +1,7 @@
-# R3F Boilerplate
+# Presentacion - Diorama 3D
 
-Este es mi punto de partida personal para todos los nuevos proyectos WebGL que utilizan el ecosistema de React Three Fiber. Está configurado con Vite para una experiencia de desarrollo rápida y moderna.
-
-### Características Incluidas
-
--   **Framework:** React 18
--   **Empaquetador:** Vite
--   **Motor 3D:** Three.js
--   **Renderizador:** React Three Fiber (`@react-three/fiber`)
--   **Utilidades:** Drei (`@react-three/drei`)
--   **Monitor de Rendimiento:** `r3f-perf`
-
-### Dependencias Principales
-
-| Paquete                 | Versión   |
-| ----------------------- | --------- |
-| `react`                 | `^18.3.1` |
-| `three`                 | `^0.164.1`|
-| `@react-three/fiber`    | `^8.16.6` |
-| `@react-three/drei`     | `^9.105.6`|
-| `r3f-perf`              | `^7.2.1`  |
-
-### Cómo Empezar
-
-1.  Clona o descarga este repositorio.
-
-2.  Instala las dependencias:
-    ```bash
-    npm install
-    ```
-3.  Ejecuta el servidor de desarrollo:
-    ```bash
-    npm run dev
-    ```
-
-
-<div align="center">
+<!-- <div align="center">
 
 ![Mi Boilerplate](boilerplate-screenshot.png)
 
-</div>
+</div> -->
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,19 +1,36 @@
-import { OrbitControls } from '@react-three/drei';
-import { Perf } from 'r3f-perf';
+import { useHelper, OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import { useRef } from 'react'
+import * as THREE from 'three'
+import { Model } from './Model'
 
 export default function Experience() 
 {
+
+    const directionalLight = useRef()
+    useHelper(directionalLight, THREE.DirectionalLightHelper, 1, 'red')
+
     return <>
+         
+        <Perf position="top-left" /> 
+        <OrbitControls makeDefault maxPolarAngle={Math.PI / 2}/>
 
-        <Perf position="top-left" />
-        <OrbitControls makeDefault />
 
-        <directionalLight position={[1, 2, 3]} intensity={4.5} />
+        <directionalLight
+            ref={ directionalLight }
+            castShadow 
+            position={ [ 1, 2, 3 ] } 
+            intensity={ 4.5 }
+            shadow-normalBias={ 0.04 }
+        />
         <ambientLight intensity={1.5} />
 
-        <mesh>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+
+        <Model />
+
+        <mesh receiveShadow rotation-x={ - Math.PI * 0.5 } scale={ 50 }>
+            <planeGeometry />
+            <meshStandardMaterial color="#a7f070" />
         </mesh>
 
     </>
